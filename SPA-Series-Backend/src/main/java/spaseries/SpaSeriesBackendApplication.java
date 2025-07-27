724714801f4ff6c417306df97aa283c3be3bf035
MMCCGG <marianocgarcia@hotmail.com>
2025-07-27 11:12:37 +0200

    Initial commit

---
+++ SPA-Series-Backend/src/main/java/spaseries/SpaSeriesBackendApplication.java
@@ -0,0 +1,13 @@
+package spaseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaSeriesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaSeriesBackendApplication.class, args);
+	}
+
+}
