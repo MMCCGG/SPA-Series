681ebf7643a6a9d7d29a7924d30f9e87bcd71440
MMCCGG <marianocgarcia@hotmail.com>
2025-08-18 17:28:34 +0200

    frontend: App+routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SeriesList } from './pages/series-list/series-list';
+import { SeriesForm } from './pages/series-form/series-form';
+import { SeriesView } from './pages/series-view/series-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "series" },
+    { path: "series", component: SeriesList },
+    { path: "nueva/serie", component: SeriesForm },
+    { path: "serie/:id", component: SeriesView},
+    { path: "actualizar/serie/:id", component: SeriesForm},
+    {path: "**", redirectTo: "series"}
+];
