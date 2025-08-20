e3863f4b219b65c5c4c871dd72f557e62220ab18
MMCCGG <marianocgarcia@hotmail.com>
2025-08-20 18:42:19 +0200

    frontend: SerieCard

--- frontend/src/app/components/serie-card/serie-card.ts
+++ frontend/src/app/components/serie-card/serie-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Serie } from '../../interfaces/serie';
+import { Botonera } from '../botonera/botonera';
 
 @Component({
   selector: 'app-serie-card',
-  imports: [],
+  imports: [Botonera],
   templateUrl: './serie-card.html',
   styleUrl: './serie-card.css'
 })
 export class SerieCard {
   
+  @Input() miSerie!: Serie;
+
 }
