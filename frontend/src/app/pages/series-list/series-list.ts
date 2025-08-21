fef3910090638b00aad6f0fa198127de2f05f234
MMCCGG <marianocgarcia@hotmail.com>
2025-08-21 16:33:48 +0200

    frontend: SeriesList

--- frontend/src/app/pages/series-list/series-list.ts
+++ frontend/src/app/pages/series-list/series-list.ts
@@ -1,11 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnDestroy } from "@angular/core";
+import { Serie } from "../../interfaces/serie";
+import { SeriesService } from "../../services/series.service";
+import { SerieCard } from "../../components/serie-card/serie-card";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-series-list",
-  imports: [],
+  imports: [SerieCard],
   templateUrl: "./series-list.html",
   styleUrl: "./series-list.css",
 })
-export class SeriesList {
-  
+export class SeriesList implements OnDestroy {
+  arrSeries: Serie[];
+  serieService = inject(SeriesService);
+  private refreshSubscription!: Subscription;
+
+  constructor() {
+    this.arrSeries = [];
+  }
+
+  ngOnInit(): void {
+    this.loadSeries();
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.refreshSubscription) {
+      this.refreshSubscription.unsubscribe();
+    }
+  }
+
+  private loadSeries(): void {
+    //Usando Observables
+    this.serieService.getAllSeries().subscribe((data: Serie[]) => {
+      this.arrSeries = data;
+    });
+    
+  }
 }
